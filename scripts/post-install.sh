b0751037dd0bae869702427fbbbd1a0fdffa388d
alekos74 <32931033+alekos74@users.noreply.github.com>
2022-08-30 12:19:27 +0200

    modifiche script post-install

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-mkdir -p src/tracedTabs
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+TRACE_DIR=/../src/tracedTabs
+mkdir -m 777 "$SCRIPT_DIR$TRACE_DIR"
